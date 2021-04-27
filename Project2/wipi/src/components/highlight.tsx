5ccd125bcc3beda5d9afbbad429028af22d006a7
jasonandjay <342690199@qq.com>
2021-04-27 09:07:40 +0800

    添加markdown高亮

--- Project2/wipi/src/components/highlight.tsx
+++ Project2/wipi/src/components/highlight.tsx
@@ -1,20 +1,36 @@
 import { useEffect, useRef } from "react";
 import hljs from 'highlight.js';
+import './highlight.less'
+import {message} from 'antd'
 
 interface IProps{
     str: string
+}
+
+function copy(value: string){
+    let textare = document.createElement('textarea');
+    document.body.appendChild(textare);
+    textare.value = value;
+    textare.select();
+    document.execCommand('copy');
+    message.success('内容已复制到剪切板');
+    textare.parentNode?.removeChild(textare);
 }
 
 const Highlight: React.FC<IProps> = props=>{
     const ref = useRef<HTMLDivElement>(null)
     useEffect(()=>{
         ref.current!.querySelectorAll('pre code').forEach((block) => {
-            console.log('block', block);
+            var copyBtn = document.createElement('button');
+            copyBtn.innerText = '复制';
+            copyBtn.classList.add('copy');
+            block.parentNode?.appendChild(copyBtn);
+            copyBtn.onclick = ()=>copy((block as HTMLElement).innerText);
             hljs.highlightBlock(block as HTMLElement);
         });
     }, [props.str])
 
-    return <div ref={ref} dangerouslySetInnerHTML={{__html: props.str}}></div>
+    return <div ref={ref} className="markdown" dangerouslySetInnerHTML={{__html: props.str}}></div>
 }
 
 export default Highlight;
